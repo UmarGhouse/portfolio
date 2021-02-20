da54371eaf96069d7cb80cc87236d02c4a50802f
Umar Ghouse <m.umarghouse@gmail.com>
2021-02-20 21:55:08 +0530

    Added more allowed attributes to SanitizeHTML
    - To fix app from crashing when it hits an attribute in a link that wasn't allowed.

--- app/javascript/components/Utilities/SanitizeHTML.js
+++ app/javascript/components/Utilities/SanitizeHTML.js
@@ -13,7 +13,7 @@ const defaultOptions = {
   ],
   disallowedTagsMode: 'discard',
   allowedAttributes: {
-    a: ['href', 'name', 'target'],
+    a: ['href', 'name', 'target', 'rel', 'title'],
     // We don't currently allow img itself by default, but this
     // would make sense if we did. You could add srcset here,
     // and if you do the URL is checked for safety
