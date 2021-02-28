870edbdbb1ca3aaba06eff11c610bac91e1dc651
Umar Ghouse <m.umarghouse@gmail.com>
2021-02-28 08:49:51 +0530

    Refactored footer and added more links
    - Created a common Link Component
    - Added links to GitHub and LinkedIn

--- app/javascript/components/Blocks/Footer.js
+++ app/javascript/components/Blocks/Footer.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 import { Grid, Typography, Link, Tooltip, Divider, Icon } from '@material-ui/core'
 import InstagramIcon from '@material-ui/icons/Instagram'
-import BookIcon from '@material-ui/icons/Book'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
 
 export default function Footer() {
+  const FooterLink = ({title, href, icon}) => (
+    <Tooltip title={title}>
+      <Link target="_blank" href={href} className="footer-link">
+        {icon}
+      </Link>
+    </Tooltip>
+  )
+
   return (
     <>
       <Divider />
@@ -19,17 +28,13 @@ export default function Footer() {
         </Grid>
 
         <Grid item>
-          <Tooltip title="My Instagram page">
-            <Link target="_blank" href="https://www.instagram.com/umarghouse/" className="footer-link">
-              <InstagramIcon />
-            </Link>
-          </Tooltip>
+          <FooterLink title="My GitHub Profile" href="https://github.com/UmarGhouse" icon={<GitHubIcon />} />
 
-          <Tooltip title="My Medium Blog">
-            <Link target="_blank" href="https://umarghouse.medium.com/" className="footer-link">
-              <Icon className="fab fa-medium-m" />
-            </Link>
-          </Tooltip>
+          <FooterLink title="My LinkedIn Profile" href="https://www.linkedin.com/in/umar-ghouse-020386a4" icon={<LinkedInIcon />} />
+
+          <FooterLink title="My Instagram Page" href="https://www.instagram.com/umarghouse/" icon={<InstagramIcon />} />
+
+          <FooterLink title="My Medium Blog" href="https://umarghouse.medium.com/" icon={<Icon className="fab fa-medium-m" />} />
         </Grid>
       </Grid>
     </>
