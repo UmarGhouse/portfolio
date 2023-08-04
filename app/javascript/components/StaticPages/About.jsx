08fb3efcb5e3e79243166548c3abdcab1f120379
Umar Ghouse <m.umarghouse@gmail.com>
2023-08-04 11:17:46 +0530

    Fix bugs, update copy and assets

--- app/javascript/components/StaticPages/About.jsx
+++ app/javascript/components/StaticPages/About.jsx
@@ -11,7 +11,7 @@ export default function About() {
       <section className="section">
         <Grid container direction="row" spacing={5} justify="space-between" alignItems="center">
           <Grid item xs={12} md={4}>
-            <img src="/assets/Profile Pic Robot-01.png" alt="Photo of me" style={{ borderRadius: "500px", width: "100%" }} />
+            <img src="/assets/profile_pic.png" alt="Photo of me" style={{ borderRadius: "500px", width: "100%" }} />
           </Grid>
 
           <Grid item xs={12} md={8}>
@@ -21,20 +21,20 @@ export default function About() {
 
             <p className='hero-body'>
               I'm Umar <span role="img">🖐🏽</span>. Self-taught developer and all-around nerd. Like I said on the{' '}
-              <Link to={"/"} component={RouterLink}>homepage</Link>, I work with Ruby-on-Rails and React to create
-              valuable web products that help people. More recently, I've also done a few projects with Node.js and Express.
+              <Link to={"/"} component={RouterLink}>homepage</Link>, I primarily work with Node.js and React to create
+              valuable web products that help people. In the past, I've also done a few projects with Ruby-on-Rails.
             </p>
           </Grid>
 
           <Grid item xs={12}>
             <p className="hero-body">
-              Many of my projects so far have been around building tools and services for clients and team mates at my current company to use.
-              This involved mailing systems, web-based platforms for hosting research and other content, a subscription management platform 
+              Many of my projects so far have been around building tools and services for clients and team mates at the companies that I've worked at.
+              This involved an Inventory Action Platform at <Link href="https://cogsy.com/" target="_blank">Cogsy</Link> and a few internal systems at <Link href="https://www.frontiergroup.info/" target="_blank">Frontier Research</Link> - namely, mailing systems, web-based platforms for hosting research and other content, a subscription management platform 
               and some scripts to help automate some tasks.
             </p>
 
             <p className="hero-body">
-              Over the years, my forays into the world of learning online have taught me web development, the basics of {' '}
+              Over the years, my forays into the world of learning online have taught me web development, some basic DevOps, the basics of {' '}
               <Link href="https://deeplearning.ai/" target="_blank">Machine Learning</Link> and more recently I'm following a foundation 
               in <Link href="https://www.edx.org/course/cs50s-introduction-to-computer-science" target="_blank">Computer Science</Link>. 
             </p>
